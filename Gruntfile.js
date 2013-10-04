95598d7d24140d4610648ad79657f9a8a420466f
Dave Murphy <dave@schwuk.com>
2013-10-04 12:10:35 +0100

    Replaced whole site with new, shiny one.

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    clean: {
+      assets: ['assets/**'],
+      site: ['_site/**']
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: '_site'
+        }
+      }
+    },
+
+    copy: {
+      bootstrap: {
+        expand: true,
+        cwd: 'bower_components/bootstrap/dist',
+        src: ['**'],
+        dest: 'assets/bootstrap'
+      },
+      html5shiv: {
+        expand: true,
+        cwd: 'bower_components/html5shiv/dist',
+        src: ['**'],
+        dest: 'assets/html5shiv'
+      },
+      jquery: {
+        expand: true,
+        cwd: 'bower_components/jquery',
+        src: ['jquery.min.js'],
+        dest: 'assets/jquery'
+      },
+      respond: {
+        expand: true,
+        cwd: 'bower_components/respond',
+        src: ['respond.min.js'],
+        dest: 'assets/respond'
+      },
+      css: {
+        expand: true,
+        cwd: 'assets/css',
+        src: ['**'],
+        dest: '_site/assets/css'
+      }
+    },
+
+    jekyll: {
+      options: {
+        config: '_config.yml',
+        src: '.'
+      },
+      site:{
+        dest: '_site'
+      }
+    },
+
+    less: {
+      development: {
+        options: {
+          paths: ['_less', 'bower_components/bootstrap/less'],
+        },
+        files: {
+          'assets/css/main.css': '_less/main.less'
+        }
+      },
+      production: {
+        options: {
+          paths: ['_less', 'bower_components/bootstrap/less'],
+          yuicompress: true
+        },
+        files: {
+          'assets/css/main.css': '_less/main.less'
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: ['_less/*.less'],
+        tasks: ['less:development', 'copy:css']
+      },
+      jekyll: {
+        files: ['_config.yml', 'index.html', '_layouts/**', '_posts/**', 'blog/**', 'events/**'],
+        tasks: ['jekyll:site']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'external']);
+  grunt.registerTask('development', ['default', 'less:development', 'jekyll:site']);
+  grunt.registerTask('external', ['copy:bootstrap', 'copy:jquery', 'copy:html5shiv', 'copy:respond'])
+  grunt.registerTask('production', ['default', 'less:production']);
+  grunt.registerTask('serve', ['development', 'connect', 'watch']);
+};
--- README.md
+++ README.md
@@ -1,22 +1,43 @@
-www.codecumbria.org
-=======
-
-Feel free to edit this website: fix any mistakes you find, add missing
-information, tweak the look 'n feel, etc.
-
-If you don't already have commit access, ping http://twitter.com/codecumbria
-
-
-
-Tips
+Code Cumbria
 ============
 
-* DO NOT optimise images/msapplication-TileImage.png. It seems there is a bug in
-  Windows that stops the icon working if it's optimised.
+[Code Cumbria](http://codecumbria.org) is an inclusive group for anyone
+involved or interested in technology, software development, and digital
+creativity in Cumbria, North-West England and South-West Scotland.
 
+This is the [Jekyll](http://jekyllrb.com/)-powered site for Code Cumbria. In
+addition [Bower](http://bower.io/) is used to manage the dependencies -
+currently [Bootstrap](http://getbootstrap.com/) and
+[jQuery](http://jquery.com/) - while [Grunt](http://gruntjs.com/) is used to
+automate the build process. In turn, both of these are managed using
+[npm](https://npmjs.org/).
 
+Getting started
+---------------
 
+Install Jekyll and [Node.js](http://nodejs.org/) - there are plenty of guides
+available so no point duplicating them here.
 
+Ensure you have `bower` and `grunt-cli` installed globally, or be prepared to
+mess around with paths!
 
+    $ npm install -g bower grunt-cli
 
-_(This one goes out to Miss Cumbria...)_
+Use `npm` to install required Node.js packages:
+
+    $ npm install
+
+Use `bower` to install dependencies:
+
+    $ bower install
+
+Modifying the site
+------------------
+
+Although you can modify this site just like any other Jekyll site, there are a
+few Grunt tasks to make life easier.
+
+ * `grunt` - the default task will clean out the `assets` folder, and re-populate it from the dependencies installed by Bower (this need to be committed alongside the rest of the project)
+ * `grunt development` - compiles the CSS (using less) and compiles the site (using Jekyll)
+ * `grunt production` - compiles and minifies the CSS, and compiles the site (use before committing)
+ * `grunt serve` - runs the development task, a webserver and watches for any changes (use for development/testing)
